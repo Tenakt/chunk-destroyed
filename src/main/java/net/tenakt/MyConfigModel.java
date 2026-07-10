d2e56f66700857f212213577d1edd1390aeeee19
Tenakt <yar.zh.2012@gmail.com>
2026-07-10 21:00:50 +0200

    gui upd

--- src/main/java/net/tenakt/MyConfigModel.java
+++ src/main/java/net/tenakt/MyConfigModel.java
@@ -8,27 +8,9 @@ import java.util.List;
 @Modmenu(modId = "chunk-destroyer")
 @Config(name = "my-config", wrapperName = "MyConfig")
 public class MyConfigModel {
-    public String firstOption = "1";
-    public int anIntOption = 16;
-    public boolean aBooleanToggle = false;
-
-    public Choices anEnumOption = Choices.ANOTHER_CHOICE;
-
-    public enum Choices {
-        A_CHOICE, ANOTHER_CHOICE;
-    }
-    @SectionHeader("someSection")
-    public String someLaterOption = "42";
-
-    @Nest
-    public ThisIsNested nestedObject = new ThisIsNested();
-
-    public static class ThisIsNested{
-        public boolean aNestedValue = false;
-        public int anotherNestedValue = 42;
-    }
 
     @RangeConstraint(min = 1,max = 128)
     public int destroyRadius = 16;
+
 
 }
